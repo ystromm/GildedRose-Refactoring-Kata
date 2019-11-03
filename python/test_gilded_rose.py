57f939a0db04eb1803503583b5e04c33764650ea
Martin Carlsson <martin@funnel.io>
2019-11-03 20:33:42 +0100

    Test for aged brie.

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -5,34 +5,41 @@ from gilded_rose import Item, GildedRose
 
 class GildedRoseTest(unittest.TestCase):
     def test_foo(self):
-        items = [Item("foo", 0, 0)]
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
-        self.assertEqual("foo", items[0].name)
+        gilded_rose = self.gilded_rose_update_quality([Item("foo", 0, 0)])
+        self.assertEqual("foo", gilded_rose.items[0].name)
 
     def test_empty_should_return_empty(self):
-        items = []
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
+        gilded_rose = self.gilded_rose_update_quality([])
         self.assertEqual([], gilded_rose.items)
 
     def test_quality_should_decrease(self):
-        items = [Item("foo", 0, 1)]
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
+        gilded_rose = self.gilded_rose_update_quality([Item("foo", 1, 1)])
         self.assertEqual(0, gilded_rose.items[0].quality)
 
+    def test_quality_should_decrease_double_if_sell_in_has_passed(self):
+        gilded_rose = self.gilded_rose_update_quality([Item("foo", 0, 10)])
+        self.assertEqual(8, gilded_rose.items[0].quality)
+
     def test_quality_should_not_become_negative(self):
-        items = [Item("foo", 0, 0)]
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
+        gilded_rose = self.gilded_rose_update_quality([Item("foo", 0, 0)])
         self.assertGreaterEqual(0, gilded_rose.items[0].quality)
 
-    def test_sellby_should_decrease(self):
-        items = [Item("foo", 1, 0)]
+    def test_sell_in_should_decrease(self):
+        gilded_rose = self.gilded_rose_update_quality([Item("foo", 1, 1)])
+        self.assertEqual(0, gilded_rose.items[0].sell_in)
+
+    def test_aged_brie_quality_should_increase_in_quality(self):
+        gilded_rose = self.gilded_rose_update_quality([Item("Aged Brie", 1, 1)])
+        self.assertEqual(2, gilded_rose.items[0].quality)
+
+    def test_aged_brie_quality_should_increase_in_quality_but_not_above_50(self):
+        gilded_rose = self.gilded_rose_update_quality([Item("Aged Brie", 1, 50)])
+        self.assertEqual(50, gilded_rose.items[0].quality)
+
+    def gilded_rose_update_quality(self, items):
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEqual(0, gilded_rose.items[0].sell_in)
+        return gilded_rose
 
 if __name__ == '__main__':
     unittest.main()
