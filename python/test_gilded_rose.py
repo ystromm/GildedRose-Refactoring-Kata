14a25b205a0e4523d7f2c1b332901f9d60492fe7
Martin Carlsson <martin@funnel.io>
2019-11-03 20:21:26 +0100

    Test for quality and sell_in.

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -10,12 +10,29 @@ class GildedRoseTest(unittest.TestCase):
         gilded_rose.update_quality()
         self.assertEqual("foo", items[0].name)
 
-    def empty_should_return_empty(self):
+    def test_empty_should_return_empty(self):
         items = []
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         self.assertEqual([], gilded_rose.items)
 
+    def test_quality_should_decrease(self):
+        items = [Item("foo", 0, 1)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(0, gilded_rose.items[0].quality)
+
+    def test_quality_should_not_become_negative(self):
+        items = [Item("foo", 0, 0)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertGreaterEqual(0, gilded_rose.items[0].quality)
+
+    def test_sellby_should_decrease(self):
+        items = [Item("foo", 1, 0)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(0, gilded_rose.items[0].sell_in)
 
 if __name__ == '__main__':
     unittest.main()
