4132a966d0fbb9bde8d831a47bd22a4886c51e2e
Martin Carlsson <martin@funnel.io>
2019-11-03 21:11:13 +0100

    Simple way of assigning zero.

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -30,7 +30,7 @@ class GildedRose(object):
                             if item.name != "Sulfuras, Hand of Ragnaros":
                                 item.quality = item.quality - 1
                     else:
-                        item.quality = item.quality - item.quality
+                        item.quality = 0
                 else:
                     if item.quality < 50:
                         item.quality = item.quality + 1
