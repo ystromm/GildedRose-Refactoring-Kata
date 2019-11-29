7c49d280aacde19ddfbbd7d13e4803f1815be606
Martin Carlsson <martin@funnel.io>
2019-11-29 17:26:07 +0100

    Added my tests, skipped tests.

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -12,6 +12,77 @@ class GildedRoseTest(TestCase):
     def setUp(self):
         self.items = []
 
+    def test_foo(self):
+        gilded_rose = self.gilded_rose_update_quality([Item("foo", 0, 0)])
+        self.assertEqual("foo", gilded_rose.items[0].name)
+
+    def test_empty_should_return_empty(self):
+        gilded_rose = self.gilded_rose_update_quality([])
+        self.assertEqual([], gilded_rose.items)
+
+    def test_quality_should_decrease(self):
+        gilded_rose = self.gilded_rose_update_quality([Item("foo", 1, 1)])
+        self.assertEqual(0, gilded_rose.items[0].quality)
+
+    def test_quality_should_decrease_double_if_sell_in_has_passed(self):
+        gilded_rose = self.gilded_rose_update_quality([Item("foo", 0, 10)])
+        self.assertEqual(8, gilded_rose.items[0].quality)
+
+    def test_quality_should_not_become_negative(self):
+        gilded_rose = self.gilded_rose_update_quality([Item("foo", 0, 0)])
+        self.assertGreaterEqual(0, gilded_rose.items[0].quality)
+
+    def test_sell_in_should_decrease(self):
+        gilded_rose = self.gilded_rose_update_quality([Item("foo", 1, 1)])
+        self.assertEqual(0, gilded_rose.items[0].sell_in)
+
+    def test_aged_brie_quality_should_increase_in_quality_after_sell_in(self):
+        gilded_rose = self.gilded_rose_update_quality([Item("Aged Brie", -1, 1)])
+        self.assertEqual(3, gilded_rose.items[0].quality)
+
+    def test_aged_brie_quality_should_increase_in_quality(self):
+        gilded_rose = self.gilded_rose_update_quality([Item("Aged Brie", 1, 1)])
+        self.assertEqual(2, gilded_rose.items[0].quality)
+
+    def test_aged_brie_quality_should_increase_in_quality_but_not_above_50(self):
+        gilded_rose = self.gilded_rose_update_quality([Item("Aged Brie", 1, 50)])
+        self.assertEqual(50, gilded_rose.items[0].quality)
+
+    def test_sulfuras_should_keep_quality(self):
+        gilded_rose = self.gilded_rose_update_quality([Item("Sulfuras, Hand of Ragnaros", 1, 80)])
+        self.assertEqual(80, gilded_rose.items[0].quality)
+
+    def test_sulfuras_should_have_quality_80(self):
+        gilded_rose = self.gilded_rose_update_quality([Item("Sulfuras, Hand of Ragnaros", 1, 1)])
+        self.assertEqual(80, gilded_rose.items[0].quality)
+
+    def test_sulfuras_should_keep_sell_in(self):
+        gilded_rose = self.gilded_rose_update_quality([Item("Sulfuras, Hand of Ragnaros", 1, 80)])
+        self.assertEqual(1, gilded_rose.items[0].sell_in)
+
+    def test_backstage_passes_should_increase_in_quality(self):
+        gilded_rose = self.gilded_rose_update_quality([Item("Backstage passes to a TAFKAL80ETC concert", 20, 1)])
+        self.assertEqual(2, gilded_rose.items[0].quality)
+
+    def test_backstage_passes_should_increase_by_2_in_quality(self):
+        gilded_rose = self.gilded_rose_update_quality([Item("Backstage passes to a TAFKAL80ETC concert", 9, 1)])
+        self.assertEqual(3, gilded_rose.items[0].quality)
+
+    def test_backstage_passes_should_increase_by_3_in_quality(self):
+        gilded_rose = self.gilded_rose_update_quality([Item("Backstage passes to a TAFKAL80ETC concert", 4, 1)])
+        self.assertEqual(4, gilded_rose.items[0].quality)
+
+    def test_backstage_passes_should_have_zero_quailty_when_sell_in_is_passed(self):
+        gilded_rose = self.gilded_rose_update_quality([Item("Backstage passes to a TAFKAL80ETC concert", 0, 10)])
+        self.assertEqual(0, gilded_rose.items[0].quality)
+
+    def gilded_rose_update_quality(self, items):
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        print(gilded_rose.items)
+        return gilded_rose
+
+    @skip
     def test_regular_items_decrease_by_one(self):
         self.items.append(Item("+5 Dexterity Vest", 10, 20))
         gilded_rose.update_quality()
@@ -20,6 +91,7 @@ class GildedRoseTest(TestCase):
         self.assertEqual(item.quality, expected['quality'])
         self.assertEqual(item.sell_in, expected['sell_in'])
 
+    @skip
     def test_quality_goes_up_for_improving_products(self):
         self.items.append(Item("Aged Brie", 20, 30))
         self.items.append(Item("Backstage passes to a TAFKAL80ETC concert", 20, 30))
@@ -34,6 +106,7 @@ class GildedRoseTest(TestCase):
             self.assertEqual(item.quality, expectation['quality'])
             self.assertEqual(item.sell_in, expectation['sell_in'])
 
+    @skip
     def test_quality_goes_up_by_two_for_improving_products_with_10_days_or_less_left(self):
         self.items.append(Item("Aged Brie", 10, 34))
         self.items.append(Item("Backstage passes to a TAFKAL80ETC concert", 8, 30))
@@ -48,6 +121,7 @@ class GildedRoseTest(TestCase):
             self.assertEqual(item.quality, expectation['quality'])
             self.assertEqual(item.sell_in, expectation['sell_in'])
 
+    @skip
     def test_quality_goes_up_by_three_for_improving_products_with_5_days_or_less_left(self):
         self.items.append(Item("Aged Brie", 4, 11))
         self.items.append(Item("Backstage passes to a TAFKAL80ETC concert", 5, 15))
@@ -62,6 +136,7 @@ class GildedRoseTest(TestCase):
             self.assertEqual(item.quality, expectation['quality'])
             self.assertEqual(item.sell_in, expectation['sell_in'])
 
+    @skip
     def test_quality_and_sellin_decrease_twice_as_fast_after_sell_by(self):
         self.items.append(Item("+5 Dexterity Vest", 0, 20))
         self.items.append(Item("Conjured Mana Cake", 0, 6))
@@ -76,6 +151,7 @@ class GildedRoseTest(TestCase):
             self.assertEqual(item.quality, expectation['quality'])
             self.assertEqual(item.sell_in, expectation['sell_in'])
 
+    @skip
     def test_backstage_passes_and_brie_go_to_quality_zero_after_sell_by(self):
         self.items.append(Item("Aged Brie", 0, 20))
         self.items.append(Item("Backstage passes to a TAFKAL80ETC concert", 0, 20))
@@ -98,6 +174,7 @@ class GildedRoseTest(TestCase):
         self.assertEqual(item.quality, expected['quality'])
         self.assertEqual(item.sell_in, expected['sell_in'])
 
+    @skip
     def test_quality_does_not_increase_past_50(self):
         self.items.append(Item("Aged Brie", 4, 49))
         gilded_rose.update_quality()
