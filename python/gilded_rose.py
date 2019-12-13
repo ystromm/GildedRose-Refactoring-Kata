e10f1bdbed2c3e73c542091b66a5f829ef2b4e11
Martin Carlsson <martin@funnel.io>
2019-12-13 11:29:42 +0100

    Removed dead code.

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -14,17 +14,8 @@ class GildedRose(object):
                     item.quality = item.quality + 1
                 item.sell_in = item.sell_in - 1
                 if item.sell_in < 0:
-                    if item.name != "Aged Brie":
-                        if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                            if item.quality > 0:
-                                item.quality = item.quality - 1
-                        else:
-                            item.quality = 0
-                    else:
-                        if item.quality < 50:
-                            item.quality = item.quality + 1
-
-
+                    if item.quality < 50:
+                        item.quality = item.quality + 1
 
             else:
                 if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
