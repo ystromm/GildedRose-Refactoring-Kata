65470e30758eb6c9f02aaab62fa8097156898391
Martin Carlsson <martin@funnel.io>
2019-12-13 11:40:14 +0100

    Cleaned up brie.

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -17,7 +17,7 @@ class GildedRose(object):
                     else:
                         item.quality += 1
             else:
-                if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
+                if item.name != "Backstage passes to a TAFKAL80ETC concert":
                     if item.quality > 0:
                         item.quality = item.quality - 1
                 else:
@@ -32,15 +32,11 @@ class GildedRose(object):
                                     item.quality += 1
                 item.sell_in = item.sell_in - 1
                 if item.sell_in < 0:
-                    if item.name != "Aged Brie":
-                        if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                            if item.quality > 0:
-                                item.quality = item.quality - 1
-                        else:
-                            item.quality = 0
+                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
+                        if item.quality > 0:
+                            item.quality = item.quality - 1
                     else:
-                        if item.quality < 50:
-                            item.quality += 1
+                        item.quality = 0
 
 
 class Item:
