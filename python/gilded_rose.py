4c0e647f44b371cb85843bd9a359393cf3d2b850
Martin Carlsson <martin@funnel.io>
2019-12-13 11:38:15 +0100

    Simplified quality calc.

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -13,23 +13,23 @@ class GildedRose(object):
                 item.sell_in = item.sell_in - 1
                 if item.quality < 50:
                     if item.sell_in < 0:
-                        item.quality = item.quality + 2
+                        item.quality += 2
                     else:
-                        item.quality = item.quality + 1
+                        item.quality += 1
             else:
                 if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
                     if item.quality > 0:
                         item.quality = item.quality - 1
                 else:
                     if item.quality < 50:
-                        item.quality = item.quality + 1
+                        item.quality += 1
                         if item.name == "Backstage passes to a TAFKAL80ETC concert":
                             if item.sell_in < 11:
                                 if item.quality < 50:
-                                    item.quality = item.quality + 1
+                                    item.quality += 1
                             if item.sell_in < 6:
                                 if item.quality < 50:
-                                    item.quality = item.quality + 1
+                                    item.quality += 1
                 item.sell_in = item.sell_in - 1
                 if item.sell_in < 0:
                     if item.name != "Aged Brie":
@@ -40,7 +40,7 @@ class GildedRose(object):
                             item.quality = 0
                     else:
                         if item.quality < 50:
-                            item.quality = item.quality + 1
+                            item.quality += 1
 
 
 class Item:
