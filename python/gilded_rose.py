4f3490b01515b6d6cf4b2d5c7f5b64320774e464
Martin Carlsson <martin@funnel.io>
2019-12-13 12:45:37 +0100

    removed some ifes

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -18,11 +18,9 @@ class GildedRose(object):
                 if item.quality < 50:
                     item.quality += 1
                     if item.sell_in < 11:
-                        if item.quality < 50:
-                            item.quality += 1
+                        item.quality += 1
                     if item.sell_in < 6:
-                        if item.quality < 50:
-                            item.quality += 1
+                        item.quality += 1
                 if item.sell_in < 0:
                     item.quality = 0
             else:
